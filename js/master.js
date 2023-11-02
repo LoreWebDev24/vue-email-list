cc57d0889443452afc0b82b55ee05172f66f042b
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-11-02 15:39:59 +0100

    JSON tests commented

--- js/master.js
+++ js/master.js
@@ -6,15 +6,24 @@ createApp({
   data() {
     return {
       emails: [],
+    //   test: `
+    //     {
+    //         "success":true,
+    //         "response": "prova@gmail.com"
+    //     }
+    //   `,
+    //   test2: 'L\'allarme'
     }
   },
   methods: {
     fetchEmails(){
         for (i=0; i < 10 ;i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
-                const email =  res.data.response
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res)=>{
+                console.log(res);
+                const email = res.data.response
             // tengo il console log solo perche' e' un esercizio
-                console.log(email) 
+                console.log(res.data); 
                 // pusho ogni email nell' array emails
                 this.emails.push(email)
         })
@@ -27,5 +36,7 @@ createApp({
   },
   mounted(){
     // console.log('Vue OK')
+    // console.log(this.test2);
+    // console.log(JSON.parse(this.test));
   }
 }).mount('#app')
